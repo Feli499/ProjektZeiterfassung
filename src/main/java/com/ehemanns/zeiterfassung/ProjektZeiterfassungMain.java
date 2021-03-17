977da3de123f418b0ac95379ca1b1f1bf97b523d
Feli <maik.ehemann@gmail.com>
2021-03-17 14:28:46 +0100

    Added main

---
+++ src/main/java/com/ehemanns/zeiterfassung/ProjektZeiterfassungMain.java
@@ -0,0 +1,11 @@
+package com.ehemanns.zeiterfassung;
+
+public class ProjektZeiterfassungMain {
+
+    /**
+     * Hier startet die Anwendung
+     */
+    public static void main(String[] args) {
+        
+    }
+}
